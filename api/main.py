9194846fef3e41af8d9bf8d67749660242a52478
Souvik Banik <souvikbanik92@gmail.com>
2024-07-22 14:53:15 +0530

    Initial commit

---
+++ api/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import os
+import uvicorn
+app = FastAPI()
+
+# Mount the static directory to serve static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Serve the index.html file at the root URL
+@app.get("/", response_class=HTMLResponse)
+async def read_index():
+    index_path = os.path.join("static", "index.html")
+    with open(index_path, 'r') as file:
+        content = file.read()
+    return HTMLResponse(content=content)
+
+if __name__ == "__main__":
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
