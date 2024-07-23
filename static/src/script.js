fe82386b6c2de6fd1dae30122bec2d0fe93aac8f
Souvik Banik <souvikbanik92@gmail.com>
2024-07-23 10:02:20 +0530

    script file

--- static/src/script.js
+++ static/src/script.js
@@ -1,18 +1,22 @@
-
 let selectedFileInput = null;
+let selectedFileInput2 = null;
+let selectedFileInput3 = null;
+document.addEventListener('DOMContentLoaded', () => {
 
-document.getElementById('input1').addEventListener('click', () => selectedFileInput = 'fileInput1');
-document.getElementById('input2').addEventListener('click', () => selectedFileInput = 'fileInput2');
-document.getElementById('input3').addEventListener('click', () => selectedFileInput = 'fileInput3');
-document.getElementById('input4').addEventListener('click', () => selectedFileInput2 = 'fileInput4');
-document.getElementById('input5').addEventListener('click', () => selectedFileInput2 = 'fileInput5');
-document.getElementById('input6').addEventListener('click', () => selectedFileInput2 = 'fileInput6');
-document.getElementById('input7').addEventListener('click', () => selectedFileInput3 = 'fileInput7');
-document.getElementById('input8').addEventListener('click', () => selectedFileInput3 = 'fileInput8');
-document.getElementById('input9').addEventListener('click', () => selectedFileInput3 = 'fileInput9');
-document.getElementById('input10').addEventListener('click', () => selectedFileInput3 = 'fileInput10');
-document.getElementById('input11').addEventListener('click', () => selectedFileInput3 = 'fileInput11');
-document.getElementById('input12').addEventListener('click', () => selectedFileInput3 = 'fileInput12');
+
+    document.getElementById('input1').addEventListener('click', () => selectedFileInput = 'fileInput1');
+    document.getElementById('input2').addEventListener('click', () => selectedFileInput = 'fileInput2');
+    document.getElementById('input3').addEventListener('click', () => selectedFileInput = 'fileInput3');
+    document.getElementById('input4').addEventListener('click', () => selectedFileInput2 = 'fileInput4');
+    document.getElementById('input5').addEventListener('click', () => selectedFileInput2 = 'fileInput5');
+    document.getElementById('input6').addEventListener('click', () => selectedFileInput2 = 'fileInput6');
+    document.getElementById('input7').addEventListener('click', () => selectedFileInput3 = 'fileInput7');
+    document.getElementById('input8').addEventListener('click', () => selectedFileInput3 = 'fileInput8');
+    document.getElementById('input9').addEventListener('click', () => selectedFileInput3 = 'fileInput9');
+    document.getElementById('input10').addEventListener('click', () => selectedFileInput3 = 'fileInput10');
+    document.getElementById('input11').addEventListener('click', () => selectedFileInput3 = 'fileInput11');
+    document.getElementById('input12').addEventListener('click', () => selectedFileInput3 = 'fileInput12');
+});
 
 async function uploadImage() {
     const resultDiv = document.getElementById('result');
@@ -34,19 +38,21 @@ async function uploadImage() {
 
     let url;
     switch (selectedFileInput) {
+
+
         case 'fileInput1':
-            url = 'http://10.240.12.205:8002/upload-img-forge/';
+            url = '/proxy/upload-img-forge/';
             resultDiv.textContent = "Processing...";
             break;
         case 'fileInput2':
-            url = 'http://10.240.12.205:8001/upload-img-df/';
+            url = '/proxy/upload-img-df/';
             resultDiv.textContent = "Processing...";
             break;
         case 'fileInput3':
-            url = 'http://10.240.12.80:8000/upload_image';
+            url = '/proxy/upload_image/';
             resultDiv.textContent = "Processing...";
             break;
-        
+
         default:
             resultDiv.textContent = "Error: Invalid input selection.";
             return;
@@ -99,7 +105,7 @@ async function uploadAudio() {
         case 'fileInput6':
             url = 'http://10.240.12.80:8000/upload_image';
             break;
-       
+
         default:
             resultDiv.textContent = "Error: Invalid input selection.";
             return;
@@ -129,7 +135,7 @@ async function uploadVideo() {
         return;
     }
 
-    const fileInput = document.getElementById(selectedFileInput2);
+    const fileInput = document.getElementById(selectedFileInput3);
     if (fileInput.files.length === 0) {
         resultDiv.textContent = "Please select a file.";
         return;
@@ -160,7 +166,7 @@ async function uploadVideo() {
         case 'fileInput12':
             url = 'http://10.240.12.80:8000/upload_image';
             break;
-       
+
         default:
             resultDiv.textContent = "Error: Invalid input selection.";
             return;
